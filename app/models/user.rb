24c4c363bad7c77339a11b48c2bd76b2825db36a
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-28 10:25:11 +0900

    userテーブルの氏名・読み仮名・パスワードにバリデーションを追加
    空欄のみのバリデーションから、氏名は全角かなカナ漢字のみ、読み仮名は全角カタカナのみ、パスワードは英数字混合出なければ登録できないように変更しました。
    手動でバリデーションが有効であることも確認しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name
-    validates :first_name
-    validates :last_name_kana
-    validates :first_name_kana
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
     validates :birthday
   end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
 
   # has_many :items
   # has_many :orders
