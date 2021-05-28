afe37cd7e483941572be73fb2b759ef66a8585b2
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-28 10:36:28 +0900

    新規登録時のエラーメッセージを追加修正
    新規登録時の入力内容に不適切な箇所があれば、エラーメッセージが表示されるように調整しました。

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,15 +6,15 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
-    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
-    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/ }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters" }
+    validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters" }
     validates :birthday
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+  validates_format_of :password, with: PASSWORD_REGEX, message: "is invalid. Include both letters and numbers"
 
   # has_many :items
   # has_many :orders
