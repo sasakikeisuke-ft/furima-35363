f8d6e66761c7566d5ac0545fc2c4306327c12c2d
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-29 18:21:14 +0900

    createアクション記載
    ccreateアクションを記載しました。それに伴いストロングパラメーターも設定しました。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -7,7 +7,19 @@ class ItemsController < ApplicationController
   end
 
   def create
-    redirect_to root_path
+    @item = Item.new(item_params)
+    if @item.valid?
+      @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+  
+  private
+
+  def item_params
+    params.require(:user).permit(:item_name, :item_description, :price, :category_id, :condition_id, :payment_id, :prefecture_id, :delivery_time_id).merge(user_id: current_user.id)
   end
 
 end
