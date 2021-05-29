e78378ffda8717a7116a6f3c3a03ad3b650e02a6
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-29 15:36:14 +0900

    itemsモデルの編集
    READMEに基づき、itemsモデルのバリデーション、アソシエーションを行いました。ordersモデルは未実装のため、コメントアウトしています。
    それに伴い、usersモデルのアソシエーションも変更しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,18 @@
 class Item < ApplicationRecord
+
+  with_options presence: true do
+    validates :item_name
+    validates :item_description
+    validates :price, numericality: { in: 300..9999999, only_integer: true }
+    validates :category_id
+    validates :condition_id
+    validates :payment_id
+    validates :delivery_time_id
+    validates :user
+  end
+
+  belongs_to :user
+  # has_one :order
+  has_one_attached :image
+
 end
