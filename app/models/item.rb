11bcd76299cd56b41833a9d4c5489bf61bec96ee
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-29 16:48:58 +0900

    itemsモデルにActiveHashのアソシエーション追記、バリデーション編集
    ActiveHashのファイルを作成したため、アソシエーションを記載しました。またバリデーションを追記しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,15 +4,26 @@ class Item < ApplicationRecord
     validates :item_name
     validates :item_description
     validates :price, numericality: { in: 300..9999999, only_integer: true }
-    validates :category_id
-    validates :condition_id
-    validates :payment_id
-    validates :delivery_time_id
     validates :user
+    with_options numericality: { other_than: 0 } do
+      validates :category_id
+      validates :condition_id
+      validates :payment_id
+      validates :prefecture_id
+      validates :delivery_time_id
+    end
   end
 
   belongs_to :user
   # has_one :order
   has_one_attached :image
+
+  # ActiveHash
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :payment
+  belongs_to :prefecture
+  belongs_to :delivery_time
 
 end
