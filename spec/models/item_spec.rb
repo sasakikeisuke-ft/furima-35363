e0550fc02669732311dfc6e0f4b7cb4b0c92e208
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-30 23:35:23 +0900

    itemモデルの単体テストコードを実装
    itemモデルの単体テストコードを実装し、問題なく通過したことを確認しました。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,115 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '商品出品機能' do
+    before do
+      @item = FactoryBot.build(:item)
+    end
+
+    context '入力内容が適切であれば登録できる' do
+      it '入力内容が全て適切であれば登録できる' do
+        expect(@item).to be_valid
+      end
+      it '商品名が40文字以内であれば登録できる' do
+        @item.item_name = Faker::Lorem.characters(number: 40)
+        expect(@item).to be_valid
+      end
+      it '商品説明が1000文字以内であれば登録できる' do
+        @item.item_description = Faker::Lorem.characters(number: 1000)
+        expect(@item).to be_valid
+      end
+    end
+    context '不適切な内容があり登録できない' do
+      it '画像データがなしだと登録できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'item_nameが空欄だと登録できない' do
+        @item.item_name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+      it 'item_descriptionが空欄だと登録できない' do
+        @item.item_description = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item description can't be blank")
+      end
+      it 'priceが空欄だと登録できない' do
+        @item.price = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+      it 'priceが半角数字でないと登録できない' do
+        @item.price = '５００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'priceが300未満だと登録できない' do
+        @item.price = '299'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'priceが10000000以上だと登録できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price is out of setting range")
+      end
+      it 'category_idが空欄だと登録できない' do
+        @item.category_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'category_idが未選択だと登録できない' do
+        @item.category_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+      it 'condition_idが空欄だと登録できない' do
+        @item.condition_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'condition_idが未選択だと登録できない' do
+        @item.condition_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+      it 'payment_idが空欄だと登録できない' do
+        @item.payment_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Payment can't be blank")
+      end
+      it 'payment_idが未選択だと登録できない' do
+        @item.payment_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Payment can't be blank")
+      end
+      it 'prefecture_idが空欄だと登録できない' do
+        @item.prefecture_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが未選択だと登録できない' do
+        @item.prefecture_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'delivery_time_idが空欄だと登録できない' do
+        @item.delivery_time_id = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+      it 'delivery_time_idが未選択だと登録できない' do
+        @item.delivery_time_id = '0'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+      it 'userが紐付けされていないと登録できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+    end
+  end
 end
