0d1ec4ec86939a29bad94c58d9014abb5790600f
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-30 09:48:33 +0900

    itemsモデルのバリデーションを修正、エラーメッセージの追加
    priceカラムのバリデーションが不適切であったため修正しました。
    合わせて、エラーメッセージが表示されるよう追記しました。

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,20 @@
 class Item < ApplicationRecord
-
   with_options presence: true do
+    validates :image
     validates :item_name
     validates :item_description
-    validates :price, numericality: { in: 300..9999999, only_integer: true }
+    validates :price,
+              format: {
+                with: /\A[0-9]+\z/,
+                message: 'is invalid. Input half-width characters'
+              },
+              numericality: {
+                greater_than_or_equal_to: 300,
+                less_than_or_equal_to: 9_999_999,
+                message: 'out of setting range'
+              }
     validates :user
-    with_options numericality: { other_than: 0 } do
+    with_options numericality: { other_than: 0, message: "can't be blank" } do
       validates :category_id
       validates :condition_id
       validates :payment_id
@@ -25,5 +34,4 @@ class Item < ApplicationRecord
   belongs_to :payment
   belongs_to :prefecture
   belongs_to :delivery_time
-
 end
