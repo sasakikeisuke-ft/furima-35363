5eb6b3a7541faf35876ad87b204d5f05ab2611c5
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-05-30 23:15:22 +0900

    FactoryBot：itemの作成
    テストコード作成の前に FactoryBotファイルを作成しました。
    また画像添付が必須のため、テスト画像を用意しました。

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,4 +1,19 @@
 FactoryBot.define do
   factory :item do
+    item_name { Faker::Name.initials(number: 2) }
+    item_description { Faker::Lorem.sentence }
+    price { Faker::Number.within(range: 300..9999999) }
+    category_id { Faker::Number.within(range: 1..10) }
+    condition_id { Faker::Number.within(range: 1..7) }
+    payment_id { Faker::Number.within(range: 1..2) }
+    prefecture_id { Faker::Number.within(range: 1..47) }
+    delivery_time_id { Faker::Number.within(range: 1..3) }
+
+    association :user
+
+    after(:build) do |target|
+      target.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
+
   end
 end
