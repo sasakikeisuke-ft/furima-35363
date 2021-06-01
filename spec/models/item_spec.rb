88195d71cf955dcb4bc32dcd2e3b7113257fbaa7
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-01 18:02:22 +0900

    Rubocop実行
    Rubocopを実行しリファクタリングを行いました。
    商品出品機能のテストコードについて修正があったため、合わせて修正しております。

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -61,7 +61,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
       it 'priceが10000000以上だと登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is out of setting range')
       end
