d41cc323c01059f487ab3247ee96910dc528fcf7
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-03 20:35:48 +0900

    商品購入ページの編集
    商品購入ページのフォーム情報を編集しました。
    それに伴い、ルーティングにcreateアクションを追加し、ordersコントローラーにcreateアクションを仮置きしました。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,11 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    
   end
 
 end
