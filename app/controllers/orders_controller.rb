6e50529e38f907fd0c95255068d12928248afd55
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-03 22:19:56 +0900

    Revert "購入機能　途中"
    This reverts commit 083a58658714f60062b1e51903cba8aede0c22d5.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,10 +6,8 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
-      card_payment
       @order_address.save
       redirect_to root_path
     else
@@ -22,16 +20,6 @@ class OrdersController < ApplicationController
   private
 
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id,item_id: item_id, token: params[:token])
-  end
-
-  def card_payment
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-    Payjp::Charge.create(
-      amount: order_params[:price], # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
-    )
   end
 
 end
