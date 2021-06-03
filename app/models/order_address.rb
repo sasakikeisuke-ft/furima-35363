4d80f97a9e66a03bbc84a43138427b12ebe1c71e
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-03 20:25:45 +0900

    Formオブジェクトを作成
    商品購入におけるレコード操作には、ordersテーブルとaddressesテーブルの二つのテーブルにまたがるため、Formオブジェクトを作成しました。

---
+++ app/models/order_address.rb
@@ -0,0 +1,21 @@
+class OrderAddress
+  include ActiveModel::Model
+  attr_accessor :item_id, :user_id, :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id
+
+  with_options presence: true do
+    validates :item_id
+    validates :user_id
+    validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validates :city
+    validates :address
+    validates :building
+    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Input only number' }
+    validates :order_id
+  end
+
+  def save
+    order = Order.create(item_id: item_id, user_id: user_id)
+    address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
+  end
+end
