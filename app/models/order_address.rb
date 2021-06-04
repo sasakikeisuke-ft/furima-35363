1741ca38716ef26f534d5fd26bd793bbd3fc3114
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-04 16:53:56 +0900

    電話番号のバリデーション記載を変更
    商品購入ページにおける電話番号の入力欄に10桁未満の数字を入力した場合、専用のエラーメッセージが出るようにバリデーションの記載を変更しました。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -10,8 +10,10 @@ class OrderAddress
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Input only number' }
-  end
+    validates :phone_number,
+      length: { in: 10..11 },
+      format: { with: /\A[0-9]\z/, message: 'is invalid. Input only number' }
+    end
 
   def save
     order = Order.create(item_id: item_id, user_id: user_id)
