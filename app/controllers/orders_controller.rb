f9f001b2bac3953abaef7a65a79adcb875048334
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-04 11:38:17 +0900

    orderコントローラーのcreateアクションを作成
    購入機能が正常に稼働するようcreateアクションを作成しました。
    その際にストロングパラメーター取得メソッドと、PAYJP使用のためのメソッドを作成しました。

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,14 +6,13 @@ class OrdersController < ApplicationController
   end
 
   def create
-    binding.pry
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       card_payment
       @order_address.save
       redirect_to root_path
     else
-      @item = Item.find(params[:item_id])
       render :index
     end
   end
@@ -22,15 +21,15 @@ class OrdersController < ApplicationController
   private
 
   def order_address_params
-    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id,item_id: item_id, token: params[:token])
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :order_id).merge(user_id: current_user.id,item_id: @item.id, token: params[:token])
   end
 
   def card_payment
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: order_params[:price], # 商品の値段
-      card: order_params[:token], # カードトークン
-      currency: 'jpy' # 通貨の種類（日本円）
+      amount: @item.price,
+      card: order_address_params[:token],
+      currency: 'jpy'
     )
   end
 
