75cdec15ac371591a508cef954db5170eaa371e9
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-04 10:40:40 +0900

    不必要なバリデーションの修正
    空欄可能なbuildingカラムに空欄禁止のバリデーションがかかっていたため、修正しました。
    また商品購入処理後に作成されるorder_idに対するバリデーションが記載されていたため削除しました。

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,9 +9,7 @@ class OrderAddress
     validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :city
     validates :address
-    validates :building
     validates :phone_number, format: { with: /\A[0-9]{10,11}\z/, message: 'is invalid. Input only number' }
-    validates :order_id
   end
 
   def save
