09217a9f2269e16ae27cf44b658b68d519e572e4
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-05 11:48:28 +0900

    order_addressモデルの単体テストコード作成
    商品購入機能において情報が正しく保存されるか、order_addressモデルの単体テストコードを作成し確認しました。

---
+++ spec/models/order_address_spec.rb
@@ -0,0 +1,97 @@
+require 'rails_helper'
+
+RSpec.describe OrderAddress, type: :model do
+  describe '商品購入機能' do
+    before do
+      @order_address = FactoryBot.build(:order_address)
+    end
+
+    context '入力内容が適切であれば登録できる' do
+      it '入力内容が全て適切であれば登録できる' do
+        expect(@order_address).to be_valid
+      end
+      it 'buildingが空欄であっても登録できる' do
+        @order_address.building = ''
+        expect(@order_address).to be_valid
+      end
+      it 'phone_numberが10桁でも登録できる' do
+        @order_address.phone_number = '0123456789'
+        expect(@order_address).to be_valid
+      end
+    end
+    context '不適切な内容があり登録できない' do
+      it 'itemが紐付けされていないと登録できない' do
+        @order_address.item_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Item can't be blank")
+      end
+      it 'userが紐付けされていないと登録できない' do
+        @order_address.user_id = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("User can't be blank")
+      end
+      it "tokenが空では登録できないこと" do
+        @order_address.token = nil
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it 'postal_codeが空欄だと登録できない' do
+        @order_address.postal_code = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code can't be blank")
+      end
+      it 'postal_codeに-が含まれないと登録できない' do
+        @order_address.postal_code = '1234567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+      it 'postal_codeにおけるハイフンの位置が、数字の3文字と4文字の間でないと登録できない' do
+        @order_address.postal_code = '1234-567'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Postal code is invalid. Enter it as follows (e.g. 123-4567)")
+      end
+      it 'prefecture_idが空欄だと登録できない' do
+        @order_address.prefecture_id = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'prefecture_idが未選択だと登録できない' do
+        @order_address.prefecture_id = 0
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it 'cityが空欄だと登録できない' do
+        @order_address.city = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("City can't be blank")
+      end
+      it 'addressが空欄だと登録できない' do
+        @order_address.address = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Address can't be blank")
+      end
+      it 'phone_numberが空欄だと登録できない' do
+        @order_address.phone_number = ''
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number can't be blank")
+      end
+      it 'phone_numberに記号が含まれると登録できない' do
+        @order_address.phone_number = '090-1111-2222'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+      it 'phone_numberに全角数字が含まれると登録できない' do
+        @order_address.phone_number = '０９０１１１１２２２２'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is invalid. Input only number")
+      end
+      it 'phone_numberが10桁未満だと登録できない' do
+        @order_address.phone_number = '123456789'
+        @order_address.valid?
+        expect(@order_address.errors.full_messages).to include("Phone number is too short (minimum is 10 characters)")
+      end
+    end
+  end
+end
+
