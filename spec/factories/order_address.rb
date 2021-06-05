6534c3662912cfe3081f61ed162e55149b8a2ae8
sasakikeisuke-ft <sasakikeisuke.ft@gmail.com>
2021-06-05 14:45:56 +0900

    修正2：外部キーを該当FactoryBotを使用し生成するように調整
    外部キーをFactoryBotから直接生成している状態から、該当FactoryBotとbefore_actionを使用し、テストコード内で生成するように調整しました。
    またcreateメソッドが重複していることによりmySQLのタイムアウトが生じることでエラーが発生しました。
    これはsleepメソッドを使用することで解消しました。

--- spec/factories/order_address.rb
+++ spec/factories/order_address.rb
@@ -1,7 +1,5 @@
 FactoryBot.define do
   factory :order_address do
-    item_id { Faker::Number.number(digits: 2) }
-    user_id { Faker::Number.number(digits: 2) }
     postal_code { '123-4567' }
     prefecture_id { Faker::Number.within(range: 1..47) }
     city { Gimei.address.city.kanji }
--- spec/models/order_address_spec.rb
+++ spec/models/order_address_spec.rb
@@ -3,7 +3,10 @@ require 'rails_helper'
 RSpec.describe OrderAddress, type: :model do
   describe '商品購入機能' do
     before do
-      @order_address = FactoryBot.build(:order_address)
+      user = FactoryBot.create(:user)
+      item = FactoryBot.create(:item)
+      @order_address = FactoryBot.build(:order_address, user_id: user.id, item_id: item.id)
+      sleep 0.01
     end
 
     context '入力内容が適切であれば登録できる' do
